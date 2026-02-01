995748a16d7d3ed00611a2f6c4877d04ceae96d7
THECODINGMASTER2014 <talwarmoksh@gmail.com>
2026-02-01 12:07:45 +0530

    Act

---
+++ Act.py
@@ -0,0 +1,7 @@
+def weather_circumstances():
+    print('The weather is pleasent in ',spring)
+    print('THe weather is the same in ',autumn)
+
+spring = "autumn"
+autumn = spring
+weather_circumstances()
